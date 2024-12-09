55c71e7c2343a99a7fb0077749f2ca22b55576c1
ichristwin <ichristwin@gmail.com>
2024-12-09 21:20:45 +0100

    minor changes

--- script/preComputeAddress.s.sol
+++ script/preComputeAddress.s.sol
@@ -9,9 +9,12 @@ contract SuperchainSolaxyDeployer is Script {
     bytes32 SALT = keccak256(abi.encodePacked("Superchain"));
 
     function run() public view {
-        console.log(unicode"⏳| Computing address with salt: ");
-        address preComputedAddress =
-            vm.computeCreate2Address(SALT, keccak256(abi.encodePacked(type(SuperchainSolaxy).creationCode)));
+        console.log(unicode"⏳| Computing address with salt: ", vm.toString(SALT));
+
+        bytes32 codeHash = keccak256(abi.encodePacked(type(SuperchainSolaxy).creationCode));
+        console.log(unicode"⏳| Code hash is: ", vm.toString(codeHash));
+
+        address preComputedAddress = vm.computeCreate2Address(SALT, codeHash);
         console.log(unicode"✨| Derived address: ", preComputedAddress);
     }
 }
